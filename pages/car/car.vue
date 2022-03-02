d1368d7519a98fc8bd05fa14ee47d9370a29b489
WestonWu <wxy5731057@vip.qq.com>
2022-03-02 14:26:21 +0800

    完成了tabbar

---
+++ pages/car/car.vue
@@ -0,0 +1,19 @@
+<template>
+  <view>
+    Car
+  </view>
+</template>
+
+<script>
+  export default {
+    data() {
+      return {
+        
+      };
+    }
+  }
+</script>
+
+<style lang="scss">
+
+</style>
